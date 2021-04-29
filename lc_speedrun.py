52db3d7fd51f7dd652e331575ef161a424651f17
Andy Olsen <andy.olsen.tx@gmail.com>
2021-04-28 20:15:46 -0500

    Keep track of total time used
    Debugging SINCE/UNTIL, not working yet.

--- lc_speedrun.py
+++ lc_speedrun.py
@@ -9,6 +9,17 @@ from datetime import datetime, timedelta
 USERNAME = "CLSmith15"
 PIECES = [chess.PAWN, chess.KNIGHT, chess.BISHOP, chess.ROOK, chess.QUEEN, chess.KING]
 SCOREBOARD = {}
+TIMEUSED = 0
+# Go to the game you want to include, look at the pgn, find these:
+#[UTCDate "2021.04.28"]
+#[UTCTime "16:33:57"]
+#SINCE = datetime.strptime("2022.04.28 17:34:59", "%Y.%m.%d %H:%M:%S")
+#SINCE = datetime.strptime("2021.04.28 16:33:58", "%Y.%m.%d %H:%M:%S")
+#SINCE = datetime.strptime("2021.04.28 16:33:57", "%Y.%m.%d %H:%M:%S")   # orig  vs prof81
+#SINCE = datetime.strptime("2021.04.28 16:33:56", "%Y.%m.%d %H:%M:%S")    # one second before vs prof81
+SINCE = datetime.strptime("2021.04.28 16:27:49", "%Y.%m.%d %H:%M:%S")    # one second before vs chingis-han
+# Use this if you want to include all games to present
+UNTIL = datetime.today()
 
 def init():
     for piece in PIECES:
@@ -18,13 +29,16 @@ def print_scoreboard():
     for piece in PIECES:
         print(SCOREBOARD[piece])
         print()
+    td = timedelta(seconds=TIMEUSED)
+    print("Total time:", td)
 
 def download_games():
-    today = datetime.today()
-    yesterday = today - timedelta(days=1)
-    response = requests.get(
-        "https://lichess.org/api/games/user/{}".format(USERNAME),
-        params={'since':yesterday.timestamp(), 'max':10, 'clocks':'true'})
+    print("since", SINCE.timestamp()*1000, SINCE)
+    print("until", UNTIL.timestamp()*1000, UNTIL)
+    params={'since':int(SINCE.timestamp()*1000), 'until':int(UNTIL.timestamp()*1000), 'max':10, 'clocks':'true'}
+    url = "https://lichess.org/api/games/user/{}".format(USERNAME)
+    print("url", url, "params", params)
+    response = requests.get(url, params=params)
     content = response.content.decode('utf-8')
     print(content)
     open("games.pgn", "w").write(content)
@@ -65,16 +79,22 @@ def parse_game(game):
     # Even if games are not useful for covering new squares,
     # the user will try to win or draw to scored time
     scored_time = max_time if user_lost else used_time
+    global TIMEUSED
+    TIMEUSED += scored_time
     #print(plies, max_time, used_time, max_time, clk_0, clk_1)
-    print("new_moves", new_moves, "used_time", used_time, "scored_time", scored_time)
+    print("new_moves", new_moves, "used_time", used_time, "scored_time", scored_time, "TIMEUSED", TIMEUSED)
+
+def parse_pgn():
+    pgn = open("games.pgn")
+    while True:
+        game = chess.pgn.read_game(pgn)
+        if not game: break
+        parse_game(game)
+    print_scoreboard()
 
 init()
 print_scoreboard()
-#download_games()
+# Comment this if you just want to reparse the games.pgn
+download_games()
+parse_pgn()
 
-pgn = open("games.pgn")
-while True:
-    game = chess.pgn.read_game(pgn)
-    if not game: break
-    parse_game(game)
-print_scoreboard()
